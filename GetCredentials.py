91517050da696319743683f39c125dc29cec2835
Jasmeet1001 <42321782+Jasmeet1001@users.noreply.github.com>
2022-04-24 22:54:26 +0530

    Delete GetCredentials.py

--- GetCredentials.py
+++
@@ -1,4 +0,0 @@
-with open(r"C:\Users\Master\Documents\ERPNotifier\usr_word.txt", 'w') as cred:
-    username = input("Enter Username/LoginID: ")
-    password = input("Enter Password: ")
-    cred.write(f"{username},{password}")
