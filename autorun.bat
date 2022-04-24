b9c206da595de8230a01d0d0920af66058837a6b
Jasmeet1001 <42321782+Jasmeet1001@users.noreply.github.com>
2022-04-24 17:03:18 +0530

    Update autorun.bat

--- autorun.bat
+++ autorun.bat
@@ -1,5 +1,5 @@
 @echo off
-set path_exe=%~dp0AssigNotify.exe
+set path_exe=%~dp0ERP_Notifier.exe
 reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v Notifier /d %path_exe%
 
 break>"C:\Users\Master\Documents\ERPNotifier\usr_word.txt"
